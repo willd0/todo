6c9d127fcb23906174b07da10e1de3f1735173cd
Will Daskam <willd2122@gmail.com>
2019-09-19 21:03:55 +0000

    Updated design

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -60,6 +60,7 @@ $(function() {
           var ulTodos = $('.todo-list');
           ulTodos.append(htmlString);
           $('.toggle').click(toggleTask);
+          $('.new-todo').val('');
         });
       });
       
